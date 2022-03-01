b8da5c2d19627e457d029fd361e3670d82629913
朱林忠 <wb_zhulinzhong@meituan.com>
2022-03-01 19:37:49 +0800

    project init

---
+++ src/main/java/com/zlz/route/RouteControllerServiceApplication.java
@@ -0,0 +1,18 @@
+package com.zlz.route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot starter
+ *
+ * @author zhulinzhong
+ */
+@SpringBootApplication
+public class RouteControllerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteControllerServiceApplication.class, args);
+    }
+
+}
